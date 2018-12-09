434b5664ae90cc171193f8af5b9c63b839784a7d
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2018-12-09 13:45:34 +0300

    Add Display generator and test

---
+++ advent/src/test/scala/org/primetalk/advent/DisplayTest.scala
@@ -0,0 +1,17 @@
+package org.primetalk.advent
+
+import org.scalatest.FlatSpec
+import org.scalatest.prop.PropertyChecks
+
+class DisplayTest extends FlatSpec with PropertyChecks with Generators {
+
+  behavior of "DisplayTest"
+
+  it should "edges" in
+    forAll(intDisplayGen){ display: Display[Int] =>
+      val edgePointValues = display.edges.map(display.apply).toSet
+      val edgePointValues2= display.valuesOnEdges
+      edgePointValues == edgePointValues2
+    }
+
+}
