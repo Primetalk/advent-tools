368a6188e80d9cd05acf363dbf186e1a5a9bac21
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2018-12-11 10:52:12 +0300

    Add day 10-11 solutions

---
+++ advent/src/test/scala/org/primetalk/advent/Day11Test.scala
@@ -0,0 +1,17 @@
+package org.primetalk.advent
+
+import org.scalatest.{FlatSpec, Matchers}
+import Day11._
+
+class Day11Test extends FlatSpec with Matchers {
+
+  behavior of "Day11Test"
+
+  it should "calculatePowerLevel" in {
+    calculatePowerLevel(8 )((  3,  5)) shouldBe 4
+    calculatePowerLevel(57)((122, 79)) shouldBe -5
+    calculatePowerLevel(39)((217,196)) shouldBe 0
+    calculatePowerLevel(71)((101,153)) shouldBe 4
+  }
+
+}
