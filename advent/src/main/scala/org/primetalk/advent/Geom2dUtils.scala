5c0a2603f942121d9b94c133a6a736b9b5773984
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2018-12-11 10:51:13 +0300

    Add rectangle and vector operations

--- advent/src/main/scala/org/primetalk/advent/Geom2dUtils.scala
+++ advent/src/main/scala/org/primetalk/advent/Geom2dUtils.scala
@@ -4,6 +4,10 @@ object Geom2dUtils {
   type Position = (Int, Int)
   type Vector = (Int, Int)
   type Direction = Vector
+  /** Origin is in top left corner. */
+  case class Rectangle(topLeft: Position, bottomRight: Position) {
+    def area: Long = {(bottomRight._1 - topLeft._1) * (bottomRight._2 - topLeft._2)}
+  }
 
   val Up: Direction = (0, +1)
   val Down: Direction = (0, -1)
@@ -19,6 +23,16 @@ object Geom2dUtils {
       (p._1 + vector._1, p._2 + vector._2)
     def -(vector: Vector): Position =
       (p._1 - vector._1, p._2 - vector._2)
+  }
+
+  implicit class VecOps(v: Vector) {
+
+    def *(k: Int): Vector = v match {
+      case (x, y) => (x * k, y * k)
+    }
+
+    def transpose: Vector =
+      (v._2, v._1)
   }
 
   val origin: Position = (0, 0)
