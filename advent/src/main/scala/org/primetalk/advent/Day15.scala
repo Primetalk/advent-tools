95274606d61d64557226c8b27d9e2191efe16878
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2018-12-23 21:37:51 +0300

    Day22: Partial solution
    It solves second part also, but getting a different result 1050.

--- advent/src/main/scala/org/primetalk/advent/Day15.scala
+++ advent/src/main/scala/org/primetalk/advent/Day15.scala
@@ -531,7 +531,6 @@ object Day15 extends Utils {
   }
 
   def round(s: State): State = {
-//    System.gc() // todo: get rid
     s.showWithUnits()
     // the order in which units take their turns within a round is
     // the reading order of their starting positions in that round
@@ -569,7 +568,7 @@ object Day15 extends Utils {
 
   // Change elfesAttackPower to 3
   // 187800
-  // 198354 - the result of this method. It returns 78 rounds instead of 77.
+  // FIXME 198354 - the result of this method.  It returns 78 rounds instead of 77.
   // 195811 - the actual expected result (rounds = 77). My solution doesn't work for it.
   //      The solution arrives at the correct position, but it takes one additional round.
   def answer1: Int = {
