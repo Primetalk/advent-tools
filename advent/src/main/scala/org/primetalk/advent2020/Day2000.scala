615618f06b9a829cfd72dbff9b764147d2e7e18f
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-12-11 09:06:33 +0300

    Add day template

---
+++ advent/src/main/scala/org/primetalk/advent2020/Day2000.scala
@@ -0,0 +1,17 @@
+package org.primetalk.advent2020
+
+/**
+  * https://adventofcode.com/2020/day/00
+  */
+object Day2000 {
+
+  lazy val answer1: Int = 0
+
+  lazy val answer2: Int = 0
+
+  def main(args: Array[String]): Unit = {
+    println("Answer1: " + answer1)
+    println("Answer2: " + answer2)
+  }
+
+}
---
+++ advent/src/test/scala/org/primetalk/advent2020/Day2000Test.scala
@@ -0,0 +1,14 @@
+package org.primetalk.advent2020
+
+import org.primetalk.advent.tools.BaseTest
+
+class Day2000Test extends BaseTest {
+
+  behavior of "Day2000Test"
+
+  it should "answers" in {
+    Day2000.answer1 shouldBe 0
+    Day2000.answer2 shouldBe 0
+  }
+
+}
