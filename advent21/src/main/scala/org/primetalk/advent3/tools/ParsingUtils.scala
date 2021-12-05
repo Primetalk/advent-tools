4313f420f87515ce89abe4b2c0f8de23ce1f61ad
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2021-12-05 09:08:02 +0300

    Make parsers static

--- advent21/src/main/scala/org/primetalk/advent3/tools/ParsingUtils.scala
+++ advent21/src/main/scala/org/primetalk/advent3/tools/ParsingUtils.scala
@@ -16,34 +16,32 @@ import cats.parse.Rfc5234
   * }}}
   */
 object ParsingUtils:
-  def a = 0
-//   type WhitespaceParser = P[_] => P[Unit]
 
-  def spaces: Parser[Unit] = Parser.char(' ').rep.void
+  val spaces: Parser[Unit] = Parser.char(' ').rep.void
 
   def ws: Parser[Unit] = (Rfc5234.wsp | Rfc5234.lf).rep.void
 
-  def decimalDigits: Parser[String] = Numbers.digits
+  val decimalDigits: Parser[String] = Numbers.digits
 
-  def positiveNumber: Parser[Int] = Numbers.nonNegativeIntString.map(_.toInt)
+  val positiveNumber: Parser[Int] = Numbers.nonNegativeIntString.map(_.toInt)
 
-  def integer = 
+  val integer = 
     ((Parser.char('-')|Parser.char('+')).?.with1 ~ decimalDigits)
       .string
       .map(_.toInt)
       //  Numbers.bigInt.map(_.toInt)
 
   // sign is mandatory
-  def signedInt = 
+  val signedInt = 
     ((Parser.char('-')|Parser.char('+')) ~ decimalDigits)
       .string
       .map(_.toInt)
 
-  def positiveLong = decimalDigits.map(_.toLong)
+  val positiveLong = decimalDigits.map(_.toLong)
 
-  def positiveBigInt = decimalDigits.map(BigInt.apply)
+  val positiveBigInt = decimalDigits.map(BigInt.apply)
 
-  def word = Parser.charWhere(_.isLetter).rep.string
+  val word = Parser.charWhere(_.isLetter).rep.string
 
   extension (str: String)
     /** Parses constant string as a fixed value.
