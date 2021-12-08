07d0bf5590dc1bde1580042bba9763a7dbc712bd
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2021-12-08 20:36:11 +0300

    Day 8

--- advent21/src/main/scala/org/primetalk/advent2021/Day2100.scala
+++ advent21/src/main/scala/org/primetalk/advent2021/Day2100.scala
@@ -1,17 +1,16 @@
 package org.primetalk.advent2021
 
-/**
-  * https://adventofcode.com/2021/day/00
-  * {{{
-  *
-  * }}}
-  */
-object Day2100:
+import org.primetalk.advent3.tools.Utils
 
-  lazy val answer1: Int = 0
+/**
+  * https://adventofcode.com/2021/day/0
+  */
+object Day2100 extends Utils:
+
+  lazy val answer1: Int = 1
 
   //Part 2
-  lazy val answer2: Int = 0
+  lazy val answer2: Int = 2
 
   def main(args: Array[String]): Unit =
     println("Answer1: " + answer1)
