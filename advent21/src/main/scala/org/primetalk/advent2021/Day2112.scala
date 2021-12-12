07ea986604db07b1cee137171fd5a0745ca4f143
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2021-12-12 22:13:51 +0300

    Day 12

---
+++ advent21/src/main/scala/org/primetalk/advent2021/Day2112.scala
@@ -0,0 +1,212 @@
+package org.primetalk.advent2021
+
+import org.primetalk.advent3.tools.Utils
+import org.primetalk.advent3.tools.GraphUtils._
+
+/**
+  * https://adventofcode.com/2021/day/12
+  * --- Day 12: Passage Pathing ---
+  * 
+  * With your submarine's subterranean subsystems subsisting suboptimally, the only way you're getting out of this cave anytime soon is by finding a path yourself. Not just a path - the only way to know if you've found the best path is to find all of them.
+  * 
+  * Fortunately, the sensors are still mostly working, and so you build a rough map of the remaining caves (your puzzle input). For example:
+  * 
+  * start-A
+  * start-b
+  * A-c
+  * A-b
+  * b-d
+  * A-end
+  * b-end
+  * 
+  * This is a list of how all of the caves are connected. You start in the cave named start, and your destination is the cave named end. An entry like b-d means that cave b is connected to cave d - that is, you can move between them.
+  * 
+  * So, the above cave system looks roughly like this:
+  * 
+  *     start
+  *     /   \
+  * c--A-----b--d
+  *     \   /
+  *      end
+  * 
+  * Your goal is to find the number of distinct paths that start at start, end at end, and don't visit small caves more than once. There are two types of caves: big caves (written in uppercase, like A) and small caves (written in lowercase, like b). It would be a waste of time to visit any small cave more than once, but big caves are large enough that it might be worth visiting them multiple times. So, all paths you find should visit small caves at most once, and can visit big caves any number of times.
+  * 
+  * Given these rules, there are 10 paths through this example cave system:
+  * 
+  * start,A,b,A,c,A,end
+  * start,A,b,A,end
+  * start,A,b,end
+  * start,A,c,A,b,A,end
+  * start,A,c,A,b,end
+  * start,A,c,A,end
+  * start,A,end
+  * start,b,A,c,A,end
+  * start,b,A,end
+  * start,b,end
+  * 
+  * (Each line in the above list corresponds to a single path; the caves visited by that path are listed in the order they are visited and separated by commas.)
+  * 
+  * Note that in this cave system, cave d is never visited by any path: to do so, cave b would need to be visited twice (once on the way to cave d and a second time when returning from cave d), and since cave b is small, this is not allowed.
+  * 
+  * Here is a slightly larger example:
+  * 
+  * dc-end
+  * HN-start
+  * start-kj
+  * dc-start
+  * dc-HN
+  * LN-dc
+  * HN-end
+  * kj-sa
+  * kj-HN
+  * kj-dc
+  * 
+  * The 19 paths through it are as follows:
+  * 
+  * start,HN,dc,HN,end
+  * start,HN,dc,HN,kj,HN,end
+  * start,HN,dc,end
+  * start,HN,dc,kj,HN,end
+  * start,HN,end
+  * start,HN,kj,HN,dc,HN,end
+  * start,HN,kj,HN,dc,end
+  * start,HN,kj,HN,end
+  * start,HN,kj,dc,HN,end
+  * start,HN,kj,dc,end
+  * start,dc,HN,end
+  * start,dc,HN,kj,HN,end
+  * start,dc,end
+  * start,dc,kj,HN,end
+  * start,kj,HN,dc,HN,end
+  * start,kj,HN,dc,end
+  * start,kj,HN,end
+  * start,kj,dc,HN,end
+  * start,kj,dc,end
+  * 
+  * Finally, this even larger example has 226 paths through it:
+  * 
+  * fs-end
+  * he-DX
+  * fs-he
+  * start-DX
+  * pj-DX
+  * end-zg
+  * zg-sl
+  * zg-pj
+  * pj-he
+  * RW-he
+  * fs-DX
+  * pj-RW
+  * zg-RW
+  * start-pj
+  * he-WI
+  * zg-he
+  * pj-fs
+  * start-RW
+  * 
+  * How many paths through this cave system are there that visit small caves at most once?
+  * 
+  * Your puzzle answer was 4411.
+  * --- Part Two ---
+  * 
+  * After reviewing the available paths, you realize you might have time to visit a single small cave twice. Specifically, big caves can be visited any number of times, a single small cave can be visited at most twice, and the remaining small caves can be visited at most once. However, the caves named start and end can only be visited exactly once each: once you leave the start cave, you may not return to it, and once you reach the end cave, the path must end immediately.
+  * 
+  * Now, the 36 possible paths through the first example above are:
+  * 
+  * start,A,b,A,b,A,c,A,end
+  * start,A,b,A,b,A,end
+  * start,A,b,A,b,end
+  * start,A,b,A,c,A,b,A,end
+  * start,A,b,A,c,A,b,end
+  * start,A,b,A,c,A,c,A,end
+  * start,A,b,A,c,A,end
+  * start,A,b,A,end
+  * start,A,b,d,b,A,c,A,end
+  * start,A,b,d,b,A,end
+  * start,A,b,d,b,end
+  * start,A,b,end
+  * start,A,c,A,b,A,b,A,end
+  * start,A,c,A,b,A,b,end
+  * start,A,c,A,b,A,c,A,end
+  * start,A,c,A,b,A,end
+  * start,A,c,A,b,d,b,A,end
+  * start,A,c,A,b,d,b,end
+  * start,A,c,A,b,end
+  * start,A,c,A,c,A,b,A,end
+  * start,A,c,A,c,A,b,end
+  * start,A,c,A,c,A,end
+  * start,A,c,A,end
+  * start,A,end
+  * start,b,A,b,A,c,A,end
+  * start,b,A,b,A,end
+  * start,b,A,b,end
+  * start,b,A,c,A,b,A,end
+  * start,b,A,c,A,b,end
+  * start,b,A,c,A,c,A,end
+  * start,b,A,c,A,end
+  * start,b,A,end
+  * start,b,d,b,A,c,A,end
+  * start,b,d,b,A,end
+  * start,b,d,b,end
+  * start,b,end
+  * 
+  * The slightly larger example above now has 103 paths through it, and the even larger example now has 3509 paths through it.
+  * 
+  * Given these new rules, how many paths through this cave system are there?
+  * 
+  * Your puzzle answer was 136767.
+  * 
+  * Both parts of this puzzle are complete! They provide two gold stars: **
+  */
+object Day2112 extends Utils:
+
+  val input = readResourceLines("day12.txt")
+
+  val edges = input.map{ case s"$s-$e" => (s,e)}.toSeq
+
+  val graph = (edges ++ edges.invert).toDirectedGraph
+
+  // 74844000, 4411
+  lazy val answer1: Int = 
+    val algs = new AllPathsSearch[String]:
+      val graphAsFunction = n => graph(n).toSeq
+      val allowedVisits = s => 
+        if s.charAt(0) >= 'A' && s.charAt(0) <= 'Z' then 
+          Int.MaxValue
+        else 
+          1
+        
+      val isFinish = _ == "end"
+
+    val paths = algs.findAllPaths(List(algs.pathInfo("start")), Nil)
+    paths.size
+
+  //Part 2 10467 158822 136767
+  def isBig(s: String)   = 
+    s.charAt(0) >= 'A' && s.charAt(0) <= 'Z'
+  def isSmall(s: String) = 
+    s.charAt(0) >= 'a' && s.charAt(0) <= 'z' && 
+    s != "start" && s != "end"
+    
+  lazy val answer2: Int =
+    def algs(allowedDup: String) = new AllPathsSearch[String]:
+      val graphAsFunction = n => graph(n).toSeq
+      val allowedVisits = s => 
+        if isBig(s) then 
+          Int.MaxValue
+        else if s == allowedDup then
+          2
+        else 
+          1
+        
+      val isFinish = _ == "end"
+    val smallCaves = graph.keySet.filter(isSmall)
+    val paths = smallCaves.flatMap(cave => 
+      val a = algs(cave)
+      a.findAllPaths(List(a.pathInfo("start")), Nil).map(_.reversePath)
+    )
+    paths.size
+
+  def main(args: Array[String]): Unit =
+    println("Answer1: " + answer1)
+    println("Answer2: " + answer2)
