d60f517f4827606eca1725785bac177759b6dd96
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2022-12-07 23:08:58 +0300

    feat: augment parsing

--- advent21/src/main/scala/org/primetalk/advent3/tools/ParsingUtils.scala
+++ advent21/src/main/scala/org/primetalk/advent3/tools/ParsingUtils.scala
@@ -43,9 +43,22 @@ object ParsingUtils:
 
   val word = Parser.charWhere(_.isLetter).rep.string
 
+  val eol = Parser.char('\n')
+
+  val stringUntilEol = Parser.charWhere(_ != '\n').string
+  
+  val endOfLine = stringUntilEol <* eol
+
   extension (str: String)
     /** Parses constant string as a fixed value.
       * Typically - enum value or ADT.
       */
     def parseAs[T](using Parser[T]): Either[cats.parse.Parser.Error, T] = 
       summon[Parser[T]].parseAll(str)
+    def parseUnsafe[T](using Parser[T]): T =
+      parseAs[T].fold(pe => 
+        throw IllegalArgumentException(
+          s"couldn't parse at offset ${pe.failedAtOffset}. "+
+          s"Expected one of (${pe.expected.toList.mkString(", ")}. "+
+          s"Offsets: ${pe.offsets.toList.mkString(",")}. Text at offset: ${str.substring(pe.failedAtOffset, math.min(pe.failedAtOffset + 10, str.length()))}"),
+       identity)
