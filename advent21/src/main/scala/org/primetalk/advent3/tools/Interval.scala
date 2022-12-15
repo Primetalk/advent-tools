bdc9241474f3d375524968ed1dcd689c88fb7b5a
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2022-12-16 00:07:36 +0300

    Day 15

---
+++ advent21/src/main/scala/org/primetalk/advent3/tools/Interval.scala
@@ -0,0 +1,61 @@
+package org.primetalk.advent3.tools
+
+case class Interval(start: Int, endInclusive: Int):
+  val size: Int = endInclusive - start + 1
+
+  def subtract(other: Interval): List[Interval] =
+    if other.start > endInclusive || start > other.endInclusive then 
+      List(this)
+    else 
+      List(
+        Interval(start, other.start - 1),
+        Interval(other.endInclusive + 1, endInclusive)
+      )
+        .filter(_.size > 0)
+
+  def intersect(other: Interval): List[Interval] =
+    if other.start > endInclusive || start > other.endInclusive then 
+      List()
+    else
+      List(Interval(math.max(start, other.start), math.min(endInclusive, other.endInclusive)))
+
+  def union(other: Interval): List[Interval] =
+    if other.start > endInclusive + 1 || start > other.endInclusive + 1 then 
+      List(this, other)
+    else
+      List(Interval(math.min(start, other.start), math.max(endInclusive, other.endInclusive)))
+
+object Interval:
+  def subtractAll(orig: List[Interval], toRemove: List[Interval]): List[Interval] =
+    toRemove match
+      case head :: tail => 
+        subtractAll(orig.flatMap(_.subtract(head)), tail)
+      case Nil =>
+        orig
+
+  // simplifies the collection of intervals to a collection of nonoverlapping intervals
+  def unionAll(intervals: List[Interval], nonOverlapping: List[Interval] = Nil): List[Interval] =
+    def mergeAdjacent(lst: List[Interval], result: List[Interval] = Nil): List[Interval] =
+      lst match
+        case head1 :: head2 :: tail => 
+          val lst = head1.union(head2)
+          lst match
+            case head :: Nil =>
+              mergeAdjacent(head :: tail, result)
+            case _ =>
+              mergeAdjacent(head2 :: tail, head1 :: result)
+        case head :: Nil => 
+          (head :: result).reverse
+        case Nil =>
+          result.reverse
+    intervals match
+      case head :: tail => 
+        //val nonOverlapping2 = nonOverlapping.foldLeft(List(head))((lst, no) => lst.flatMap(no.union(_)))
+        val nonOverlapping2 = 
+          (subtractAll(List(head), nonOverlapping) reverse_::: nonOverlapping)
+            .sortBy(_.start)
+        unionAll(tail, mergeAdjacent(nonOverlapping2))
+        //unionAll(tail, mergeAdjacent(head::nonOverlapping.flatMap(_.union(head)))
+      case Nil =>
+        nonOverlapping
+    
