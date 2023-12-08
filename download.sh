6144478b93950048172af6110fb07b19eb9637f3
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2023-12-08 20:54:02 +0300

    Day 6 input

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
-echo Downloading Day$((2200+$1)).txt
-curl -H "Cookie: session=$TOKEN" https://adventofcode.com/2022/day/$1/input > advent21/src/main/resources/org/primetalk/advent2022/Day$((2200+$1)).txt
+echo Downloading Day$((2300+$1)).txt
+curl -H "Cookie: session=$TOKEN" https://adventofcode.com/2023/day/$1/input > advent21/src/main/resources/org/primetalk/advent2023/Day$((2300+$1)).txt
