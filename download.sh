436c4dba43a6462ffed20727059efeba08eb860d
Arseniy Zhizhelev <a.zhizhelev@tinkoff.ru>
2023-12-16 21:04:04 +0300

    Use bash directly

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
-#!/bin/env bash
+#!/bin/bash
 echo Downloading Day$((2300+$1)).txt
 curl -H "Cookie: session=$TOKEN" https://adventofcode.com/2023/day/$1/input > advent21/src/main/resources/org/primetalk/advent2023/Day$((2300+$1)).txt
